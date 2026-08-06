cc11c8fb45490f39479d7d1ca38c9423afd8c6e6
89285-Salonee <shirsatsalonee510@gmail.com>
2026-08-06 15:08:46 +0530

    initialize forum service

--- forum-service/src/main/java/com/pravesh/forum/ForumServiceApplication.java
+++ forum-service/src/main/java/com/pravesh/forum/ForumServiceApplication.java
@@ -1,13 +1,10 @@
 package com.pravesh.forum;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@SpringBootApplication
-@EnableFeignClients
 public class ForumServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ForumServiceApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
